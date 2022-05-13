bdc9092903c896d50b17542ef84d3141831985fe
unknown <muhaimin.habib077@gmail.com>
2022-05-13 11:14:16 +0600

    cleaned the search.js file

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,28 +4,10 @@ const Search = ({ blogs }) => {
     const [searchPattern, setSearchPattern] = useState('');
     const [matchedBlogs, setMatchedBlogs] = useState('')
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log("i will handle the search")
-    // }
-
-    // array.reverse() to reverse an array
-        const string = "hello i am habib";
-        // console.log(string.match());
-
-    // useEffect(() => {
-    //     console.log(searchPattern)
-    //     let searchString = `/${searchPattern}/i`;
-    //     console.log(blogs.filter(blog => (blog.title.match(searchString) !== null)))
-    // }, [searchPattern]);
-
-    //`\\b${searchPattern}\\b
-
     useEffect(() => {
         console.log(searchPattern)
-        // let searchString = /searchPattern/i;
         let searchString = new RegExp(searchPattern, 'i');
-        console.log(blogs.filter(blog => (searchString.test(blog.title))))
+        setMatchedBlogs(blogs.filter(blog => (searchString.test(blog.title))))
     }, [searchPattern]);
 
     return (
@@ -46,7 +28,6 @@ const Search = ({ blogs }) => {
                         />
                     </div>
                 </div>
-                {/* <button type='submit' onClick={handleSubmit}>Search</button> */}
             </form>
         </div>
     )
